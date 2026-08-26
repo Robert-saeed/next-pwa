e8f651aa0d2da1384b0cab1f14241dd61587e918
Robert <robert.sassd.00@gmail.com>
2026-08-26 14:50:08 +0300

    update

--- app/sw.ts
+++ app/sw.ts
@@ -2,13 +2,9 @@ import { defaultCache } from "@serwist/next/worker";
 import type { PrecacheEntry, SerwistGlobalConfig } from "serwist";
 import { CacheFirst, NetworkFirst, Serwist, StaleWhileRevalidate } from "serwist";
 
-declare global {
-  interface WorkerGlobalScope extends SerwistGlobalConfig {
-    __SW_MANIFEST: (string | PrecacheEntry)[] | undefined;
-  }
-}
-
-declare const self: ServiceWorkerGlobalScope;
+declare const self: ServiceWorkerGlobalScope & {
+  __SW_MANIFEST: (string | PrecacheEntry)[] | undefined;
+};
 
 const serwist = new Serwist({
   precacheEntries: self.__SW_MANIFEST,
